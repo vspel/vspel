206f7ccbf0668e59d5cf8ddece8bf40210e51fa3
bmlevchenko <bmlevchenko@gmail.com>
2015-12-16 19:02:27 +0200

    Initial app structure

---
+++ src/main/java/org/vspel/VspelApplication.java
@@ -0,0 +1,12 @@
+package org.vspel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VspelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VspelApplication.class, args);
+    }
+}
