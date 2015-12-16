29539981a4760cbdf490dc4fee164ac9d8693aa0
bmlevchenko <bmlevchenko@gmail.com>
2015-12-16 19:12:03 +0200

    Rename entry point class

--- src/main/java/org/vspel/Application.java
+++ src/main/java/org/vspel/Application.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class VspelApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(VspelApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 }
