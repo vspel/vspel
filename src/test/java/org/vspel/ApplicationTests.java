718f986a1faba77833f23f9fb5fd37981f827faf
bmlevchenko <bmlevchenko@gmail.com>
2015-12-16 19:33:40 +0200

    Add h2 for integration tests

--- src/test/java/org/vspel/ApplicationTests.java
+++ src/test/java/org/vspel/ApplicationTests.java
@@ -2,13 +2,16 @@ package org.vspel;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = Application.class)
 @WebAppConfiguration
+@SpringApplicationConfiguration(classes = Application.class)
+@PropertySources(value = @PropertySource("application-test.properties"))
 public class ApplicationTests {
 
 	@Test
